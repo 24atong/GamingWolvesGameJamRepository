af9c6997413a21da6beda09e9c13e04eda8c9fe9
24atong <78821792+24atong@users.noreply.github.com>
2022-04-13 17:06:36 -0400

    Reverting finishing level sound should work now

--- GamingWolvesGameJamUnityProject/Assets/Scripts/FinishLineScript.cs
+++ GamingWolvesGameJamUnityProject/Assets/Scripts/FinishLineScript.cs
@@ -8,7 +8,10 @@ public class FinishLineScript : MonoBehaviour
     {
         GameObject.Find("Player").SendMessage("Finish");
 <<<<<<< HEAD
+<<<<<<< HEAD
         FindObjectOfType<AudioManager>().Play("LevelComplete");
+=======
+>>>>>>> parent of d677fa8 (finishing level sound should work now)
         SceneManager.LoadScene(1);
 =======
 >>>>>>> parent of 582d66e (Added a new scene to act as a menu when you finish the level. You now get the option to either play again or to quit game if you win.)
